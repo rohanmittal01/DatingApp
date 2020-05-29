573cff4a5724de106c4f2ca28a72a129b140933c
rohanmittal01 <rohanmittal01@gmail.com>
2020-05-30 02:02:31 +0530

    error handling completed

---
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { AlertifyService } from '../_services/Alertify.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  model: any = {};
+
+  constructor(public authService: AuthService, private alertify: AlertifyService, private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  login(){
+    // console.log(this.model);
+    this.authService.login(this.model).subscribe(next => {
+      this.alertify.success('Logged in successfully');
+
+    }, error => {
+      this.alertify.error(error);
+      // console.log("failed to login");
+    }, () => {
+      this.router.navigate(['/members']);
+    });
+  }
+
+loggedIn(){
+  return this.authService.loggedIn();
+}
+
+logout(){
+  localStorage.removeItem('token');
+  this.alertify.warning('Logged Out!');
+  this.router.navigate(['/home']);
+}
+}
